53882c0545ea0fc8f4360d882bb34aaae8e0bb02
hyperalchemy <23337041+hyperalchemy@users.noreply.github.com>
2022-11-19 11:42:03 +0100

    started on mappings

--- src/font-project.ts
+++ src/font-project.ts
@@ -1,15 +1,24 @@
 import { Address, ipfs, json } from "@graphprotocol/graph-ts";
 import {
   FontProjectMinted,
-  NewFontProjectCreated
+  NewFontProjectCreated,
+  NewFontProjectCreated__Params,
+  FontProject__idToFontProjectResult,
+
 } from "../generated/FontProject/FontProject"
 import { Query, User, Link, FontProject, FontStream, FontStreamFund, FontStreamCollaboration} from "../generated/schema"
 import { integer } from "@protofire/subgraph-toolkit";
 
 export function handleNewFontProjectCreated(event: NewFontProjectCreated): void { 
-  // TO DO 
+  let NewFontProjectCreated = FontProject.load(event.params.fontId.toHex());
+    if (NewFontProjectCreated == null) {
+    NewFontProjectCreated = new FontProject(event.params.fontId.toHex());
+
+    NewFontProjectCreated.save();
+
+    }
 }
-export function handleFontProjectMinted(event: FontProjectMinted): void { 
-  // TO DO 
-}
+// export function handleFontProjectMinted(event: FontProjectMinted): void { 
+//   // TO DO 
+// }
 ;
